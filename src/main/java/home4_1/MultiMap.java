63ec1b6b49c5bbdf53bb93743ae883e01ddb0cf0
Konstantin <che.kt@mail.ru>
2016-08-17 15:25:31 +0700

    add int

--- src/main/java/home4_1/MultiMap.java
+++ src/main/java/home4_1/MultiMap.java
@@ -1,6 +1,7 @@
 package home4_1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Airo on 17.08.2016.
@@ -9,5 +10,7 @@ public class MultiMap {
     public static void main(String[] args) {
         HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
         ArrayList<Object> arrayList = new ArrayList<>();
+        int c = 0;
+        int b = 1;
     }
 }
