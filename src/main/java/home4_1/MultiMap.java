7eccb97dd9eaff27ab91d257533c735e401c8c8e
Konstantin <che.kt@mail.ru>
2016-08-17 13:53:10 +0700

    add HashMap

--- src/main/java/home4_1/MultiMap.java
+++ src/main/java/home4_1/MultiMap.java
@@ -1,10 +1,12 @@
 package home4_1;
 
+import java.util.HashMap;
+
 /**
  * Created by Airo on 17.08.2016.
  */
 public class MultiMap {
     public static void main(String[] args) {
-        
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
     }
 }
