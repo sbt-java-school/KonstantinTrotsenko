ca8a1f9f9619958036790242db80585cc089a177
Konstantin <che.kt@mail.ru>
2016-08-17 15:35:44 +0700

    change MultiMap

--- src/main/java/home4_1/MultiMap.java
+++ src/main/java/home4_1/MultiMap.java
@@ -7,10 +7,5 @@ import java.util.HashMap;
  * Created by Airo on 17.08.2016.
  */
 public class MultiMap {
-    public static void main(String[] args) {
-        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
-        ArrayList<Object> arrayList = new ArrayList<>();
-        int c = 0;
-        int b = 1;
-    }
+
 }
